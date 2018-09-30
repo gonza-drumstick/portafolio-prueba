e94363a4b3730eb5f641436de3553634ff2c6fec
Gonzalo Mattos <gonza.drumstick@gmail.com>
2018-09-30 20:08:42 -0300

    prueba

--- js/funny-text.js
+++
@@ -1,8 +0,0 @@
-$('.profesion').funnyText({
-		speed: 500,
-		borderColor: 'green',
-		activeColor: 'white',
-		color: 'black',
-		fontSize: '17em',
-		direction: 'both'
-	});
---
+++ js/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+        //-----NAVEGACION-----
+
+        var inicio    = $('#inicio').offset().top,
+        	contacto  = $('#contacto').offset().top,
+			acercaDe  = $('#acerca-de').offset().top,
+			trabajos  = $('#trabajos').offset().top;
+			htmlYbody = $('html, body')
+
+        $('#btn-inicio').on('click', function(){
+        	
+        	htmlYbody.animate({
+        		scrollTop: inicio 
+        	},1500);
+        });
+
+        $('#btn-contacto').on('click', function(){
+        	
+        	htmlYbody.animate({
+        		scrollTop: contacto
+        	},1500);
+        });
+        
+        $('#btn-acerca-de').on('click', function(){
+        	
+        	htmlYbody.animate({
+        		scrollTop: acercaDe - 100
+        	},1500);
+        });
+        
+        $('#btn-trabajos').on('click', function(){
+        	
+        	htmlYbody.animate({
+        		scrollTop: trabajos - 50
+        	},1500);
+        });
+
+
+
+        // -----MENU PEGAJOSO-----
+
+        var altura = $('.menu').offset().top;
+		var menu = $('.menu');
+
+		$(window).on('scroll', function(){
+			if ( $(window).scrollTop() > altura ){
+				menu.addClass('menu-fixed');
+
+			} else {
+				menu.removeClass('menu-fixed');
+			}
+		});
+
+	
+});
